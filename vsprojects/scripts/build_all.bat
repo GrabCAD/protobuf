3479190163ae6e4bd515ab50607f0146a87a5cee
Frank Lindeman <frank@grabcad.com>
2015-08-12 14:07:10 +0100

    Update version

--- vsprojects/scripts/build_all.bat
+++ vsprojects/scripts/build_all.bat
@@ -1,10 +1,20 @@
 @echo off
 
+@echo Start building solution
 "C:\Program Files (x86)\MSBuild\12.0\bin\MSBuild.exe" protobuf.sln /t:Build /p:PlatformToolset=v120 /p:Configuration=Debug /p:Platform=x64
 "C:\Program Files (x86)\MSBuild\12.0\bin\MSBuild.exe" protobuf.sln /t:Build /p:PlatformToolset=v120 /p:Configuration=Release /p:Platform=x64
 "C:\Program Files (x86)\MSBuild\12.0\bin\MSBuild.exe" protobuf.sln /t:Build /p:PlatformToolset=v120 /p:Configuration=Debug /p:Platform=Win32
 "C:\Program Files (x86)\MSBuild\12.0\bin\MSBuild.exe" protobuf.sln /t:Build /p:PlatformToolset=v120 /p:Configuration=Release /p:Platform=Win32
+@echo Finished building solution
 
-extract_includes
+@echo Start extracting includes
+call extract_includes.bat
+@echo Finished extracting includes
 
+@echo Start deleting NuGet packages
+del *.nupkg
+@echo Finished deleting NuGet packages
+
+@echo Start building NuGet package
 PowerShell -Command "& { .\scripts\generate_package.ps1 }"
+@echo Finished building NuGet package
